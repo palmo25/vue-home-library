bd073ca5b03f00bbe5825c5192e12c50f8e7b0b8
Sandro Palmieri <sandropalmieri@outlook.com>
2017-01-27 10:38:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+    <h1>{{ msg }}</h1>
+     <Booklist />
+  </div>
+</template>
+
+<script>
+import Booklist from './components/Booklist.vue'
+export default {
+  name: 'app',
+  data () {
+    return {
+      msg: 'Welcome to Your Vue.js Home Library'
+    }
+  },
+  components:{Booklist}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1, h2 {
+  font-weight: normal;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+</style>
---
+++ src/components/Booklist.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="">
+    <h2>Booklist</h2>
+    <ul>
+      <li v-for="book in books">{{book.name}}</li>
+    </ul>
+  </div>
+
+</template>
+
+<script>
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+
+export default {
+  name: "Booklist",
+  data() {
+    return{
+        books: []
+    }
+
+  },
+  beforeMount(){
+    Vue.axios.get("http://localhost:3000/books.json").then((response) => {
+      this.books = response.data
+  })
+}
+}
+</script>
+
+<style scoped>
+
+</style>
